64cf0d27347f799a9bb02a87aeb10381f41e125c
Андрей Тамбовцев <andrtamb@mail.ru>
2025-04-30 00:49:57 +0300

    small update

--- obj/Debug/net9.0/AtomyWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/AtomyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtomyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2dcf1331922f21a40781eacf3dd0312723d415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfab019e8ecbcf9375542656444fbfff0e73b632")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtomyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtomyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
